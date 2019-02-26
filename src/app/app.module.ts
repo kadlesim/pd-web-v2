72e4b02ae094f16d1a8c30908180eb3a0a2cd94d
Simon Kadlec <simon.kadlec@se-va.cz>
2019-02-26 16:56:07 +0100

    started import from pd-web; started adding Material Design - theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,108 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { EventComponent } from './event/event.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { LoginComponent } from './login/login.component';
+import { MediaComponent } from './media/media.component';
+import { PlayerListComponent } from './player-list/player-list.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
+import { ResultListComponent } from './result-list/result-list.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {EventListService} from './event-list/event-list.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'blog',
+    children: [
+      {
+        path: '',
+        component: BlogComponent
+      },
+      {
+        path: 'blog-post/:id',
+        component: BlogPostComponent
+      }
+    ]
+  },
+  {
+    path: 'blog-post/:title/:image/:text',
+    component: BlogPostComponent
+  },
+  {
+    path: 'player-list',
+    component: PlayerListComponent
+  },
+  {
+    path: 'event-list',
+    component: EventListComponent,
+    resolve: {
+      // events: EventsResolver
+    }
+  },
+  {
+    path: 'media',
+    component: MediaComponent,
+    // canActivate: [AuthGuard]
+  },
+  {
+    path: 'result-list',
+    component: ResultListComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'player-detail',
+    component: PlayerDetailComponent
+  },
+  { path: '',
+    // redirectTo: '/home',
+    component: HomeComponent
+    // pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    BlogPostComponent,
+    EventComponent,
+    EventListComponent,
+    LoginComponent,
+    MediaComponent,
+    PlayerListComponent,
+    PlayerDetailComponent,
+    ResultListComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    EventListService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
