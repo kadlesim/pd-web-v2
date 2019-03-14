fcf4a50e038d44a6f94993eae660add35687232f
Simon Kadlec <simon.kadlec@se-va.cz>
2019-03-14 15:46:59 +0100

    home page styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Directive, ElementRef, HostListener, Renderer} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Prague Devils';
+
+  constructor(private el: ElementRef, private renderer: Renderer) {}
+
 }
