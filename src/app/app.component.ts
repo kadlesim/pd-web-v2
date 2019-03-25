31d16162f7a442a4ddd4371784692e3d17a6f111
Simon Kadlec <simon.kadlec@se-va.cz>
2019-03-25 16:45:26 +0100

    shrinkable header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, Directive, ElementRef, HostListener, Renderer} from '@angular/core';
 
+declare const window: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +10,23 @@ import {Component, Directive, ElementRef, HostListener, Renderer} from '@angular
 export class AppComponent {
   title = 'Prague Devils';
 
-  constructor(private el: ElementRef, private renderer: Renderer) {}
+  constructor() {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const num = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const shrinkableHeader = document.getElementById('shrinkable-header');
+    const pdLogo = document.getElementById('pd-logo');
+    if (num > 50) {
+      console.log('You are 100px from the top to bottom');
+      shrinkableHeader.setAttribute('style', 'height: 7vh;');
+      pdLogo.setAttribute('style', 'height: 5vh;');
+
+    } else if (num <= 50) {
+      console.log('You are 500px from the top to bottom');
+      shrinkableHeader.setAttribute('style', 'height: 10vh;');
+      pdLogo.setAttribute('style', 'height: 7vh;');
+    }
+  }
 
 }
